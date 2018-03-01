c743b689118baf31a06f260d7597f595a911ed67
ibisha <16367098+ibisha@users.noreply.github.com>
2018-03-01 23:33:21 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+const http = require('http');
+const server = http.createServer((req, res) => {
+  const now = new Date();
+  console.info('[' + now + '] Requested');
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8'
+  });
+  switch (req.method) {
+    case 'GET':
+      const fs = require('fs');
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res);
+      break;
+    case 'POST':
+      let body = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body);
+        const decoded = decodeURIComponent(body);
+        console.info('[' + now + '] 投稿: ' + decoded);
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+        decoded + 'が投稿されました</h1></body></html>')
+        res.end();
+      });
+      break;
+    default:
+      break;
+  }
+}).on('error', (e) => {
+  console.error('[' + new Date() + '] Server Error', e);
+}).on('clientError', (e) => {
+  console.error('[' + new Date() + '] Server Error', e);
+});
+const port = 8000;
+server.listen(port, () => {
+  console.log('Listening on ' + port);
+});
